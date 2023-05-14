c4d9f4a56f871241b715da4dfaa78fc495b1226e
Raghav <kumarraghav71950@gmail.com>
2023-05-14 12:39:51 +0530

    adding controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -23,6 +23,39 @@ Post request json file structure
 const registerUser =async (req, res) => {
 
     //write your code here
+    var name = req.body.name;
+
+    var email  = req.body.email;
+
+    var password = req.body.password;
+
+    var DOB = req.body.DOB;
+
+    var newuser = {
+
+        "name":name,
+
+        "email":email,
+
+        "password": password,
+
+        "DOB": DOB,
+
+    };
+
+    // console.log(newuser);
+
+    users.create(newuser).then((user) => {
+
+        res.send(user._id);
+
+    })
+
+    .catch((error) => {
+
+        res.status(404).send(error.message);
+
+    });
 
 }
 
